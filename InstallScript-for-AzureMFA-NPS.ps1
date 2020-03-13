41bcfee81f1f6fb217c0a479cafba13131e2e2dc
Daniel Weppeler <daniel.weppeler@braincon.de>
2020-03-14 00:15:34 +0100

    first upload and readme changes

---
+++ InstallScript-for-AzureMFA-NPS.ps1
@@ -0,0 +1,53 @@
+﻿param([Parameter(Mandatory=$true)][string]$RadiusClientName="", [Parameter(Mandatory=$true)][string]$RadiusClientIP="")
+
+# Install AzureMFA
+# (c) Daniel Weppeler 13.03.2020 v0.75 
+# Twitter: @_DanielWep
+# freeware license.
+
+Write-Host("Info: Set the ExecutionPolicy to unrestricted") -ForegroundColor Green 
+Set-ExecutionPolicy Unrestricted -Force
+Write-Host("Info: Install NPS Feature") -ForegroundColor Green 
+
+$install = Install-WindowsFeature NPAS -IncludeManagementTools
+$os=(Get-WMIObject win32_operatingsystem).name
+
+If($install.Success -eq "true") {
+    Write-Host("Info: Installation was successful with status code "+ $install.ExitCode +".") -ForegroundColor Green 
+    $adiuscliensecret = Read-Host -Prompt 'Please enter your Radius SharedSecret' -AsSecureString
+    $RadiusClientSecret = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($RadiusClientSecret))
+        try {
+        $client = New-NpsRadiusClient -Address $RadiusClientIP -Name $RadiusClientName -SharedSecret $RadiusClientSecret
+
+        Write-Host("Info: New RadiusClient " + $RadiusClientName + "with IP-Adresse " + $RadiusClientIP + " was added successful.") -ForegroundColor Green 
+        $domainregister = netsh nps add registeredserver
+            If ($domainregister -eq "Ok.") {
+            Write-Host("Info: Server " + $env:computername + " was succesful registered to domain "+ (Get-WmiObject Win32_ComputerSystem).Domain + ".") -ForegroundColor Green 
+                try {
+                    Write-Host("Info: Downloading Extension for Azure MFA") -ForegroundColor Green 
+                    Invoke-WebRequest -Uri "https://download.microsoft.com/download/B/F/F/BFFB4F12-9C09-4DBC-A4AF-08E51875EEA9/NpsExtnForAzureMfaInstaller.exe" -OutFile ($env:USERPROFILE + "\Downloads\NpsExtnForAzureMfaInstaller.exe")
+                    Start-Process -Wait -FilePath ($env:USERPROFILE + "\Downloads\NpsExtnForAzureMfaInstaller.exe") -ArgumentList "/S" -PassThru
+                    Write-Host("Info: Extension for Azure MFA is installed.") -ForegroundColor Green
+                    Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+                    Write-Host("Info: NuGet is installed.") -ForegroundColor Green
+                    Set-PSRepository -InstallationPolicy Trusted -name PSGallery
+                    & ($env:ProgramFiles+"\Microsoft\AzureMfa\Config\AzureMfaNpsExtnConfigSetup.ps1")
+                        if ($os -match "2019") {
+                            New-NetFirewallRule -DisplayName "Allow-Radius-UDP" -Direction Inbound -LocalPort 1812,1813 -Protocol UDP -Action Allow
+                            Write-Host("Info: New Firewall Rule for Allow Radius traffic is installed.") -ForegroundColor Green
+                            } 
+
+                    } catch {
+                        Write-Host("Error: " + $_.Exception.Message) -ForegroundColor Red 
+                        }
+            }
+            
+
+
+
+        } catch {
+            Write-Host("Error: " + $_.Exception.Message) -ForegroundColor Red 
+            }
+    } else {
+    Write-Host("Error: Installation was aborted with status code "+ $install.ExitCode) -ForegroundColor Red 
+}
--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-# InstallScript-for-AzureMFA-NPS
+# Install Script for Microsoft Network Policy Server (NPS) with AzureMFA Integration
+
+```
+- Installing the WindowsFeature Network Policy Server (NPS)
+- A Radius Client will be added, e.g. NetScaler Subnet IP
+- Downloading and Installing the AzureMFA NPS Extension
+- Execute the initial AzureMFA config Script
+```
+
+## Getting Started
+
+The Install Scripte should be run on the destination NPS Server.
+
+### Prerequisites
+
+```
+- Licenses for Azure Multi-Factor Authentication (included with Azure AD Premium, EMS, or an MFA stand-alone license)
+- Windows Server 2008 R2 SP1 or above.
+- Network connectivity to the following destination with port 80/443 (Webproxy also possible or direct):
+	- https://adnotifications.windowsazure.com
+	- https://login.microsoftonline.com
+	- https://login.microsoftonline.com
+	- https://provisioningapi.microsoftonline.com
+	- https://aadcdn.msauth.net
+- Active Directory domain-joined server
+```
+
+## Running the scripts
+
+### 
+
+```
+InstallScript-for-AzureMFA-NPS -RadiusClientName [YourRadiusClientName] -RadiusClientIP [YourRadiusClientIP]
+```
+
+## PostActions
+
+```
+- Create NPS Policies manually for Connection and Network request
+- Check if your new NPS Server added to the ActiveDirectory RAS and IAS Server Group. Otherwise you can authorize the server via NPS console right-click on the server and choose "Register server in Active Directory"
+```
+
+#
